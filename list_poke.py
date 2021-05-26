5eb42dab8b774433691443287db176c59a069ab8
carlosbrito20 <80795522+carlosbrito20@users.noreply.github.com>
2021-05-25 21:56:42 -0300

    Delete list_poke.py

--- list_poke.py
+++
@@ -1,5 +0,0 @@
-list_poke = [
-    {'total': 1000, 'limit': 10, 'offset': 10, 'data': [
-        {"id": 1, "name": "ditto", "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"},
-        {"id": 2, "name": "charmander", "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"}
-    ]}]
